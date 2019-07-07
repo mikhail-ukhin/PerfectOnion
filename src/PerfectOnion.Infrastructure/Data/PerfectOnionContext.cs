9504b477845e86829e6f8417693d468c5ab9ac62
Mikhail Ukhin <mikhail.ukhin@gmail.com>
2019-07-07 15:54:26 +0300

    CustomerConfiguration

--- src/PerfectOnion.Infrastructure/Data/PerfectOnionContext.cs
+++ src/PerfectOnion.Infrastructure/Data/PerfectOnionContext.cs
@@ -1,7 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using PerfectOnion.Infrastructure.EntityConfigurations;
+
 namespace PerfectOnion.Infrastructure.Data
 {
-    public class PerfectOnionContext
+    public class PerfectOnionContext : DbContext
     {
-        
+        public PerfectOnionContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+        }
     }
 }
---
+++ src/PerfectOnion.Infrastructure/EntityConfigurations/CustomerConfiguration.cs
@@ -0,0 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PerfectOnion.Core.Entities;
+
+namespace PerfectOnion.Infrastructure.EntityConfigurations
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.ToTable("Customers");
+            
+            builder
+                .HasKey(e => e.Id);
+
+            builder
+                .Property(e => e.Id)
+                .HasColumnName("Id")
+                .ValueGeneratedOnAdd();
+            
+            builder
+                .Property(e => e.Address)
+                .HasColumnName("Address")
+                .HasMaxLength(512);
+
+            builder
+                .Property(e => e.Email)
+                .HasColumnName("Email")
+                .IsRequired()
+                .HasMaxLength(128);
+
+            builder
+                .Property(e => e.Phone)
+                .HasColumnName("Phone")
+                .IsRequired()
+                .HasMaxLength(64);
+
+            builder
+                .Property(e => e.FirstName)
+                .HasColumnName("FirstName")
+                .IsRequired()
+                .HasMaxLength(64);
+
+            builder
+                .Property(e => e.MiddleName)
+                .HasColumnName("MiddleName")
+                .HasMaxLength(64);
+
+            builder
+                .Property(e => e.LastName)
+                .HasColumnName("LastName")
+                .HasMaxLength(64);          
+        }
+    }
+}
